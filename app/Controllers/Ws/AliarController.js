a0eaa10d4194c1007199c5ea5dc269ae4938fd7a
oil <le.rue@protonmail.com>
2020-01-15 10:16:35 -0500

    'zzz'

---
+++ app/Controllers/Ws/AliarController.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class AliarController {
+  constructor ({ socket, request }) {
+    this.socket = socket
+    this.request = request
+  }
+     onMessage (message) {
+    this.socket.broadcastToAll('message', message)
+  }
+      onClose () {
+    // same as: socket.on('close')
+  }
+
+  onError () {
+    // same as: socket.on('error')
+  }
+}
+
+module.exports = AliarController
